111381372f3a74839da5a31474b71abf9e76f6db
Daniel-Magar <daniel.t@ekoinfomatics.com>
2022-09-12 00:01:26 +0530

    boxes

--- src/components/MultiCheckboxes.jsx
+++ src/components/MultiCheckboxes.jsx
@@ -29,20 +29,27 @@ const handleChange =(e)=> {
  }
 
 }
+const clearAll = (e) => {
+  e.preventDefault();
+  let tempUser = users.map((user)=> {
+    return {...user, isChecked:false};
+  });
+  setUsers(tempUser);
+}
     return(
         <div>
             <form action="">
                 <h3>Select Users</h3>
                 <div>
-                    <label>
-                        <input type="checkbox" className="" name="selectall" onChange={handleChange} checked={users.filter((user)=>user?.isChecked!=true).length<1}/>
+                    <label className="mx-2 cursor-pointer">
+                        <input type="checkbox" className="mx-4 cursor-pointer" name="selectall" onChange={handleChange} checked={users.filter((user)=>user?.isChecked!=true).length<1}/>
                         Select All
                     </label>
                 </div>
                 {users.map((data, index) => (
                     <div className="flex gap-10">
                         <div className="flex" key={index}>
-                        <label>
+                        <label className="mx-2 cursor-pointer ">
                             <input type="checkbox" 
                             name={data.name}
                             onChange={handleChange}
@@ -57,7 +64,9 @@ const handleChange =(e)=> {
                     ):""}
                     </div>
                 ))}
-               
+               <div className="border-b p-3">
+                <button className="p-2 px-10 bg-teal-500 text-[white] cursor-pointer" onClick={clearAll}>Clear</button>
+               </div>
             </form>
         </div>
     )
